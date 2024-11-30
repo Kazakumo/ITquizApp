52ad6b71e8ac82f70e5c6e1d8e9b4b50471e57f3
Kazakumo <kenmonkenmon@outlook.jp>
2024-11-30 21:04:56 +0900

    feature: ページの外形を実装

---
+++ frontend/src/app/(pages)/OurQuiz/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { Button, Container, Fieldset, Heading } from "@yamada-ui/react";
+export const OurQuiz = () => {
+	return (
+		<>
+			<Container centerContent={true}>
+				<Heading>問1 ほげほげとは何か</Heading>
+				<Fieldset legend="1" errorMessage="不正解です。">
+					<Button variant={"ghost"} width={"full"}>
+						1. hogehogehguaghuag
+					</Button>
+				</Fieldset>
+				<Fieldset legend="2" errorMessage="不正解です。">
+					<Button variant={"ghost"} width={"full"}>
+						2. hogehogehguaghuag
+					</Button>
+				</Fieldset>
+				<Fieldset legend="3">
+					<Button variant={"ghost"} width={"full"}>
+						3. hogehogehguaghuag
+					</Button>
+				</Fieldset>
+				<Fieldset legend="4">
+					<Button variant={"ghost"} width={"full"}>
+						4. hogehogehguaghuag
+					</Button>
+				</Fieldset>
+			</Container>
+		</>
+	);
+};
+export default OurQuiz;
