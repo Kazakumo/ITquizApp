471e7306bfb5fb2c689e8a8b8376607a709cf2ba
Kazakumo <shunya.sato1402@gmail.com>
2025-06-23 01:23:13 +0900

    feat: ルートページのサーバーサイドでセッションチェックする機構を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 "use server";
 
+import { redirect } from "next/navigation";
 import { SignIn } from "@/app/_features/SignIn";
+import { auth } from "@/auth";
 
 export default async function Home() {
+	const session = await auth();
+	if (session) {
+		redirect("/home");
+	}
 	return (
 		<>
 			<SignIn />
