5319bfc69668a944e2b41e2d227747373a87e302
Kazakumo <shunya.sato1402@gmail.com>
2025-06-23 01:24:11 +0900

    feat: signinページの実装

--- src/app/_features/SignIn/SignIn.tsx
+++ src/app/_features/SignIn/SignIn.tsx
@@ -1,42 +1,19 @@
-import { auth, signIn, signOut } from "@/auth";
+import { signIn } from "@/auth";
 
 export const SignIn = async () => {
-	const session = await auth();
-
 	return (
 		<>
-			{!session && (
-				<form>
-					<button
-						type="button"
-						onClick={async () => {
-							"use server";
-							await signIn("GitHub", { redirectTo: "/home" });
-						}}
-					>
-						Sign in with GitHub
-					</button>
-				</form>
-			)}
-			{session && (
-				<form
-					action={async () => {
+			<form>
+				<button
+					type="button"
+					onClick={async () => {
 						"use server";
-						console.log("signOut");
-						await signOut();
+						await signIn("GitHub", { redirectTo: "/home" });
 					}}
 				>
-					<button
-						type="button"
-						onClick={async () => {
-							"use server";
-							await signOut();
-						}}
-					>
-						Sign out
-					</button>
-				</form>
-			)}
+					Sign in with GitHub
+				</button>
+			</form>
 		</>
 	);
 };
